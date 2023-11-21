3e8a26296df6a3b8d049a516bc45a4c37b7a02c5
Tobias Fenner <tfenner@usc.edu>
2023-11-20 20:46:47 -0800

    openai api call

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,9 +1,39 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
 import io
 import csv
 from flask import Response
+import requests
+import json
+
+# Your API key
+api_key = "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"
+
+# Endpoint URL
+endpoint_url = "https://api.openai.com/v1/chat/completions"
+
+# Data payload for the request
+data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "user", "content": "Say this is a test!"}],
+    "temperature": 0.7
+}
+
+# Headers with your API key
+headers = {
+    "Authorization": f"Bearer {api_key}",
+    "Content-Type": "application/json"
+}
+
+# Make the POST request to the API
+response = requests.post(endpoint_url, json=data, headers=headers)
+
+# Get the response JSON
+response_json = response.json()
+
+# Print the response
+print(response_json)
 
 cred = credentials.Certificate("celeb-data-a3288-firebase-adminsdk-2ujgb-bfddc51066.json")
 firebase_admin.initialize_app(cred)
@@ -27,6 +57,39 @@ def index():
     data = [doc.to_dict() for doc in items]
 
     return render_template('index.html', collections=collections, selected_collection=selected_collection, items=data)
+
+# Function to call OpenAI API
+def call_openai_api():
+
+    # Endpoint URL
+    endpoint_url = "https://api.openai.com/v1/chat/completions"
+
+    # Data payload for the request
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "Say this is a test!"}],
+        "temperature": 0.7
+    }
+
+    # Headers with your API key
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+
+    # Make the POST request to the API
+    response = requests.post(endpoint_url, json=data, headers=headers)
+
+    # Return the response JSON
+    return response.json()
+
+@app.route('/process_pdf', methods=['POST'])
+def process_pdf():
+    # Call the OpenAI API
+    response_json = call_openai_api()
+
+    # Return the response
+    return jsonify(response_json)
 
 @app.route('/download/<collection_name>')
 def download_csv(collection_name):
