8bb8025a5ea7e9200e86dee78cf4f8bde45e0d32
Tobias Fenner <tfenner@usc.edu>
2023-11-20 17:36:51 -0800

    im frontendmaxxing

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -1,27 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for
 import firebase_admin
 from firebase_admin import credentials, firestore
+import io
+import csv
+from flask import Response
 
-# Initialize Firebase Admin
 cred = credentials.Certificate("celeb-data-a3288-firebase-adminsdk-2ujgb-bfddc51066.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-# Initialize Flask App
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    collections = ['justin bieber', 'paris hilton', 'ice spice']
+    data = {}
+
     if request.method == 'POST':
-        item = request.form['item']
-        db.collection('items').add({'item': item})
+        for collection in collections:
+            item = request.form.get(f'item_{collection}')
+            if item:
+                db.collection(collection).add({'item': item})
+
         return redirect(url_for('index'))
 
-    docs = db.collection('items').stream()
-    items = [doc.to_dict() for doc in docs]
-    return render_template('index.html', items=items)
+    for collection in collections:
+        docs = db.collection(collection).stream()
+        data[collection] = [doc.to_dict() for doc in docs]
 
+    return render_template('index.html', data=data)
+
+@app.route('/download/<collection_name>')
+def download_csv(collection_name):
+    # Fetch data from Firestore
+    docs = db.collection(collection_name).stream()
+    data = [doc.to_dict() for doc in docs]
+
+    # Create a CSV in-memory
+    proxy = io.StringIO()
+    writer = csv.writer(proxy)
+    writer.writerow(['Item'])  # Header row, adjust as needed
+
+    for item in data:
+        writer.writerow([item['item']])  # Adjust based on your data structure
+
+    # Create a response
+    proxy.seek(0)
+    response = Response(proxy, mimetype='text/csv')
+    response.headers['Content-Disposition'] = f'attachment;filename={collection_name}.csv'
+
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
